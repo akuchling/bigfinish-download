e6dc7ee0c306a06923323a67a1ac98851d766df9
Andrew Kuchling <amk@amk.ca>
2014-01-27 20:04:39 -0500

    Finish first draft of downloading logic
    We do HEAD operations to get the filenames and record them in the
    self.titles dictionary.  download_audio() can then check for the
    existence of the filenames and download the ones that aren't present.

--- bf-download.py
+++ bf-download.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import re
 import sys
+import os
+import json
 import requests
 import bs4
 
@@ -12,9 +14,23 @@ class Downloader:
         self.session = requests.Session()
         self.titles = {}
 
+    def read_persistent_data(self):
+        json_file = os.path.join(self.args.dir, '.library.json')
+        if os.path.exists(json_file):
+            with open(json_file, 'r') as f:
+                d = json.load(f)
+                # Merge contents of the dictionary into the .titles dict
+                self.titles.clear()
+                self.titles.update(d)
+
+    def write_persistent_data(self):
+        json_file = os.path.join(self.args.dir, '.library.json')
+        with open(json_file, 'w') as f:
+            json.dump(self.titles, f, indent='  ')
+
     def login(self):
         # Log in to the server
-        if True:
+        if not self.args.dry_run:
             if self.args.verbose:
                 sys.stderr.write('Logging into server...\n')
 
@@ -29,16 +45,17 @@ class Downloader:
                                     'data[remember_me]': '1'},
                           )
 
-    def read_library(self):
+    def read_purchased_library(self):
         # Read the complete library page
-        if self.args.verbose:
-            print('Retrieving complete library...')
-        if True:
+        html_file = os.path.join(self.args.dir, '.library.html')
+        if not self.args.dry_run:
+            if self.args.verbose:
+                print('Retrieving complete library...')
             r = self.session.get('http://bigfinish.com/customers/my_account/perpage:0')
             html = r.text
-            open('bf-download-new.html', 'w').write(html)
+            open(html_file, 'w').write(html)
         else:
-            html = open('bf-download-new.html', 'rb').read()
+            html = open(html_file, 'r').read()
 
         # Parse the HTML.
         if self.args.verbose:
@@ -75,11 +92,90 @@ class Downloader:
         if self.args.verbose:
             sys.stderr.write('%i titles in library\n' % len(self.titles))
 
-        # Check for actual file name of titles
+        # Ensure all the titles have '*_filename' set to None if it's not
+        # present.
+        for d in self.titles.values():
+            d.setdefault('audiobook_filename', None)
+            d.setdefault('mp3_filename', None)
+
         return html_parser
 
+    def determine_filenames(self):
+        """Loop over contents of self.titles and figure out the filenames we
+        want to download.
+        """
+        for k, d in sorted(self.titles.items()):
+            url = None
+            for format in (self.args.prefer_format, 'mp3', 'audiobook'):
+                if format in d:
+                    url = d[format]
+                    filename_key = format + '_filename'
+                    break
+
+            assert url is not None, 'No URL found for {0}'.format(k)
+
+            # Do we already know the filename?
+            if d[filename_key]:
+                continue
+
+            # Not present, so do an HTTP HEAD to get the filename.
+            if self.args.verbose:
+                print('Determining filename for {!r}'.format(k))
+            if self.args.dry_run:
+                continue
+            req = self.session.head(url)
+            content_disp = req.headers['Content-disposition']
+            m = re.search('filename=" ( [^"]* )"', content_disp, re.VERBOSE)
+            assert m is not None, "Header {0} doesn't contain a filename".format(content_disp)
+            filename = m.group(1)
+            d[filename_key] = filename
+            if self.args.verbose:
+                print('  ... will download filename {!r}'.format(filename))
+
     def download_audio(self):
-        pass
+        "Download audio zip files"
+        # This method shouldn't modify the .titles dictionary, because it
+        # will have already been written out to disk at this point.
+        for k, d in sorted(self.titles.items()):
+            url = None
+            for format in (self.args.prefer_format, 'mp3', 'audiobook'):
+                if format in d:
+                    url = d[format]
+                    filename_key = format + '_filename'
+                    filename = d[filename_key]
+                    break
+
+            assert url is not None, 'No URL found for {0}'.format(k)
+
+            path = os.path.join(self.args.dir, filename)
+            if os.path.exists(path):
+                continue
+
+            if self.args.verbose:
+                print('Retrieving {} file for {!r}: {}'.format(format, k,
+                                                               filename))
+            if self.args.dry_run:
+                continue
+
+            # Download the archive
+            # XXX it would be politer to save a partially-downloaded
+            # file and then resume it.
+            try:
+                input = self.session.get(url, stream=True)
+                # XXX could check that the length matches afterwards
+                length = int(input.headers['Content-Length'])
+                with open(path, 'wb') as output:
+                    for chunk in input.iter_content(4096):
+                        output.write(chunk)
+            except Exception:
+                print('Error: removing {}'.format(path))
+                os.remove(path)
+                raise
+            except KeyboardInterrupt:
+                print('Interrupted: removing {}'.format(path))
+                os.remove(path)
+                raise
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -88,24 +184,33 @@ def main():
                         help='bigfinish.com user account name')
     parser.add_argument('password', metavar='PASSWORD',
                         help='bigfinish.com user account password')
-    parser.add_argument('dir', metavar='DIRECTORY',
+    parser.add_argument('dir', metavar='DIRECTORY', default='.',
                         help='directory to store audio files')
     parser.add_argument('--prefer-audiobook', action='store_const',
                         const='audiobook', dest='prefer_format',
+                        default='mp3',
                         help='download audiobook format when available')
     parser.add_argument('--prefer-mp3', action='store_const',
                         const='mp3', dest='prefer_format',
                         default='mp3',
                         help='download mp3s when available')
+    parser.add_argument('-n', '--dry-run', action='store_true',
+                        dest='dry_run', default=False,
+                        help='produce more logging output')
     parser.add_argument('-v', '--verbose', action='store_true',
-                        dest='verbose',
+                        dest='verbose', default=False,
                         help='produce more logging output')
     args = parser.parse_args()
 
     dl = Downloader(args)
     dl.login()
-    dl.read_library()
+    dl.read_persistent_data()
+    dl.read_purchased_library()
+    dl.determine_filenames()
+    dl.write_persistent_data()
     dl.download_audio()
+    return dl
+
 
 if __name__ == '__main__':
     hp = main()
